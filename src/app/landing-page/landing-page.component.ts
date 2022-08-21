eb3ef9494ecbfedfab8db248a83312e1bbb42183
CodeSysteme <co26teme@gmail.com>
2022-08-21 18:17:56 +0100

    add form template and reactive

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,12 +9,18 @@ import { Router } from '@angular/router';
 })
 export class LandingPageComponent implements OnInit {
 
+  userEmail!: string;
+
   constructor(private router: Router) { }
 
   ngOnInit(): void { }
 
   onContinue(): void {
     this.router.navigateByUrl('facesnaps');
+  }
+
+  onSubmit(formEmail: NgForm): void {
+    console.log(formEmail.value);
   }
 
 }
